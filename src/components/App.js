4bdfa34ebc40e2755be03f854baab4cf746bfbce
오기택 <kitaiki@spring.local>
2017-12-11 15:11:12 +0900

    reat folow project 1 - 주소록

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Contact from './Contact'
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: ''
+        };
+    }
+
+    render(){
+
+        return (
+            <Contact/>
+        );
+    }
+}
+
+export default App;
